628a93f2eafb53449cef287fca5f07aa182435a7
jimmynXD <87709880+jimmynXD@users.noreply.github.com>
2023-05-17 17:06:17 -0400

    feat: new map

--- apps/web/src/modules/map/pages/mapPage.tsx
+++ apps/web/src/modules/map/pages/mapPage.tsx
@@ -1,15 +1,9 @@
 import { GoogleMap, Marker, useLoadScript } from '@react-google-maps/api'
 import { useMemo, useState } from 'react'
-import usePlacesAutocomplete, {
-  getGeocode,
-  getLatLng,
-} from 'use-places-autocomplete'
 
 export function MapPage() {
   const { isLoaded } = useLoadScript({
-    googleMapsApiKey:
-      process.env.GOOGLE_MAPS_API_KEY ??
-      'AIzaSyAQe4q2QbqCVCzqfUqyVbvLaZ1TyaDPpiE',
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY ?? '',
     libraries: ['places'],
   })
   const [selected, setSelected] = useState(null)
@@ -18,96 +12,21 @@ export function MapPage() {
 
   if (!isLoaded) return <div>loading...</div>
 
+  const options = {
+    mapTypeControlOptions: {
+      position: google.maps.ControlPosition.BOTTOM_LEFT,
+    },
+  }
   return (
-    <div className="min-h-screen flex flex-col relative">
-      <div className="z-10 fixed top-4 left-1/2 -translate-x-1/2">
-        <PlacesAutocomplete setSelected={setSelected} />
-      </div>
+    <div className="h-screen relative">
       <GoogleMap
-        mapContainerClassName="w-full h-full flex-1"
+        options={options}
+        mapContainerClassName="w-full h-full"
         center={selected ?? center}
         zoom={10}
       >
         {selected && <Marker position={selected} />}
       </GoogleMap>
-    </div>
-  )
-}
-
-interface PlacesAutocompleteProps {
-  setSelected: React.Dispatch<React.SetStateAction<any>>
-}
-const PlacesAutocomplete = ({ setSelected }: PlacesAutocompleteProps) => {
-  const {
-    ready,
-    value,
-    suggestions: { status, data },
-    setValue,
-    clearSuggestions,
-  } = usePlacesAutocomplete({
-    requestOptions: {
-      /* Define search scope here */
-    },
-    debounce: 300,
-  })
-  const handleInput = (e) => {
-    // Update the keyword of the input element
-    setValue(e.target.value)
-  }
-
-  const handleSelect =
-    ({ description }) =>
-    () => {
-      // When user selects a place, we can replace the keyword without request data from API
-      // by setting the second parameter to "false"
-      setValue(description, false)
-      clearSuggestions()
-
-      // Get latitude and longitude via utility functions
-      getGeocode({ address: description }).then((results) => {
-        const { lat, lng } = getLatLng(results[0])
-        setSelected({ lat, lng })
-      })
-    }
-  const renderSuggestions = () =>
-    data.map((suggestion) => {
-      const {
-        place_id,
-        structured_formatting: { main_text, secondary_text },
-      } = suggestion
-
-      return (
-        <li key={place_id} onClick={handleSelect(suggestion)}>
-          <strong>{main_text}</strong> <small>{secondary_text}</small>
-        </li>
-      )
-    })
-  return (
-    // <ComboBox
-    //   placeholder="Search a restaurant"
-    //   defaultItems={status === 'OK' ? data : []}
-    //   inputValue={value}
-    //   onSelectionChange={(item) => {
-    //     console.log(item)
-    //   }}
-    //   onInputChange={(e: string) => setValue(e)}
-    // >
-    //   {data.map(({ place_id, description }) => (
-    //     <Item key={place_id}>
-    //       {place_id}
-    //       {description}
-    //     </Item>
-    //   ))}
-    // </ComboBox>
-    <div className="bg-white">
-      <input
-        value={value}
-        onChange={handleInput}
-        disabled={!ready}
-        placeholder="Where are you going?"
-      />
-      {/* We can use the "status" to decide whether we should display the dropdown or not */}
-      <ul>{renderSuggestions()}</ul>
     </div>
   )
 }
